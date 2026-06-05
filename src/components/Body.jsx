fe12604381a31b3cfded3fc506f8ae90a33e8dca
Prashansha Neupane <neupane.prashansha2005@gmail.com>
2026-06-05 18:34:22 +0545

    set up useState counter with button click handler

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,30 @@
 
 import ProductCard from './ProductCard.jsx';
 import '../styles/Body.css';
-import { products } from "../constants/constant.js"
+import { products } from "../constants/constant.js";
+import { useState} from 'react';
 
 const Body = () => {
+  const [productList,setProductList] = useState(products);
+  const[element, setElement] = useState(4);
+
+  function handleTopRatedProducts(){
+    const topRatedProducts = products.filter((product) => product.rating.rate > 4);
+    setProductList(topRatedProducts);
+  }
+
+  function handleAdd() {
+    setElement(element + 1);
+  }
+
   return (
     <main className="product-cards">
-      {products.map((product) => (
+      <div>
+        <button onClick={handleTopRatedProducts}>Top Rated Products</button>
+        <button onClick={handleAdd}>Add</button>
+        <h2>{element}</h2>
+      </div>
+      {productList.map((product) => (
         <ProductCard data={product} key={product.id} />
       ))}
     </main>
